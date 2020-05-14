5606f3b386b576192ad339040601804add9abaea
Thibaut STRULLU <thibaut.strullu@aurera.fr>
2020-05-15 00:33:31 +0200

    update config mailchimp

--- src/website-config.ts
+++ src/website-config.ts
@@ -58,11 +58,11 @@ const config: WebsiteConfig = {
   facebook: 'https://www.facebook.com/',
   twitter: 'https://twitter.com/',
   showSubscribe: true,
-  mailchimpAction: '',
-  mailchimpName: '',
-  mailchimpEmailFieldName: '',
+  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
+  mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: '',
+  footer: 'created by thibaut',
 };
 
 export default config;
